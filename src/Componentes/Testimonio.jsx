a79ce370f7dfe25a0d5a1b15b114f80459ea9f94
Mriano29 <mriano290801@gmail.com>
2024-10-17 20:37:20 +0100

    Proyecto modificado y terminado!

--- src/Componentes/Testimonio.jsx
+++ src/Componentes/Testimonio.jsx
@@ -3,19 +3,26 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
-import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { useState } from 'react';
+import {MiguelRR} from 'milibreria';
+import {MyFavourite} from 'milibreria';
+import {MyFavouriteBorder} from 'milibreria';
+import {MyCustomButton} from 'milibreria';
 
 export default function Testimonio({imageUrl,imageAlt ,name, location, description, testimony }) {
     const[like, setLike] = useState(false);
+
+    // Función añadida para comprobar el funcionamiento del boton
+    const handleClick = () => {
+      alert('Efectivamente, funciona!');
+    };
 
     const handleLike = () => {
         setLike(like => !like);
       };
       
   return (
+    
     <Card sx={{ width: 400, margin:2}}>
         <CardMedia
         component="img"
@@ -25,8 +32,16 @@ export default function Testimonio({imageUrl,imageAlt ,name, location, descripti
       />
       <CardContent>
         <Typography gutterBottom variant="h4" component="div">
-          {name} en {location}
+          
         </Typography>
+        {/* Uso del componente MiguelRR el cual es un typography */}
+        <MiguelRR
+          text={`${name} en ${location}`}
+          alignment="center"
+          color="success"
+          bottomMargin
+          variant="h4"
+        />
         <Typography variant="subtitle1" sx={{ color: 'ActiveBorder' }}>
           {description}
         </Typography>
@@ -35,10 +50,18 @@ export default function Testimonio({imageUrl,imageAlt ,name, location, descripti
         </Typography>
       </CardContent>
       <CardActions>
-      <IconButton aria-label="add to favorites" onClick={handleLike}>
-          {like === true ? <FavoriteIcon sx={{color: 'red'}}/>: <FavoriteBorderIcon sx={{color: 'red'}}/>}
-        </IconButton>
-      </CardActions>
+        {/* Uso de los componentes MyFavourite y MyFavouriteBorder para mostrar el icono de corazon lleno y vacío */}
+          {like === true ? <MyFavourite iconColor='error'  onClick={handleLike}/>: <MyFavouriteBorder iconColor='error'  onClick={handleLike}/>}
+         {/* Uso del componente MyCustomButton*/}
+          <MyCustomButton
+              text='Soy un botón'
+              txtcolor='white'
+              bgcolor='cyan'
+              size='small'
+              hoverColor='red'
+              onClick={handleClick}
+          />
+      </CardActions>        
     </Card>
   );
 }
